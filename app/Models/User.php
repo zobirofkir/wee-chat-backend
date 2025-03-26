97b987f6762762ec23b705cc10837f8c237f1162
zobirofkir <zobirofkir19@gmail.com>
2025-03-26 15:17:23 +0000

    create users migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
+        'avatar',
+        'phone',
         'password',
     ];
 
