0f7d93a3c70940ba6f3456759d043539242b2fed
zobirofkir <zobirofkir19@gmail.com>
2025-03-26 15:21:21 +0000

    create register resource

---
+++ app/Http/Resources/RegisterResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegisterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'phone' => $this->phone,
+            'avatar' => $this->avatar,
+        ];
+    }
+}
