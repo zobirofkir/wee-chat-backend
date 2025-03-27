b7ad5298c02136e75d01a51761396df98cd02cdb
zobirofkir <zobirofkir19@gmail.com>
2025-03-27 05:56:54 +0000

    clean code

--- app/Services/Constructors/Auth/ForgetPasswordConstructor.php
+++ app/Services/Constructors/Auth/ForgetPasswordConstructor.php
@@ -2,7 +2,17 @@
 
 namespace App\Services\Constructors\Auth;
 
+use App\Http\Requests\Auth\ForgotPasswordRequest;
+use App\Http\Requests\Auth\ResetPasswordRequest;
+use Illuminate\Http\Request;
+
 interface ForgetPasswordConstructor
 {
-    //
+    public function sendResetLinkEmail(ForgotPasswordRequest $request);
+
+    public function showResetForm(Request $request, $token = null);
+
+    public function resetPassword(ResetPasswordRequest $request);
+
+    public function __construct();
 }
