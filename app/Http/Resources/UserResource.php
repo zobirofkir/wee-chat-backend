3a7cc57e6f785c3c16680fbda4874e36d373213d
zobirofkir <zobirofkir19@gmail.com>
2025-03-27 03:31:57 +0000

    create show and delete current authentacted user

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'phone' => $this->phone,
+            'avatar' => asset('storage/' . $this->avatar),
+        ];
+    }
+}
--- app/Services/Constructors/Auth/AuthConstructor.php
+++ app/Services/Constructors/Auth/AuthConstructor.php
@@ -2,7 +2,24 @@
 
 namespace App\Services\Constructors\Auth;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
 interface AuthConstructor
 {
-    //
+    /**
+     * Show authenticated user
+     *
+     * @param User $user
+     * @return UserResource
+     */
+    public function show(User $user) : UserResource;
+
+    /**
+     * Delete Current Authenticated User Account
+     *
+     * @param User $user
+     * @return boolean
+     */
+    public function delete(User $user) : bool;
 }
