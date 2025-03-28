b3a3a714555a98cea5f83ea68353b5adde6246e5
zobirofkir <zobirofkir19@gmail.com>
2025-03-28 22:12:21 +0000

    fix error from show and delete current authenticated user

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,29 +3,30 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Http\Resources\UserResource;
 use App\Services\Facades\Auth\AuthFacade;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     /**
      * Show current authenticated user
      *
-     * @return mixed
+     * @return UserResource
      */
-    public function show()
+    public function show() : UserResource
     {
-        return AuthFacade::show(auth()->user());
+        return AuthFacade::show(Auth::user());
     }
 
     /**
      * Delete current authenticated user
      *
-     * @return mixed
+     * @return bool
      */
-    public function delete()
+    public function delete() : bool
     {
-        return AuthFacade::delete(auth()->user());
+        return AuthFacade::delete(Auth::user());
     }
 }
