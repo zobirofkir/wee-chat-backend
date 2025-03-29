2b1d8ac9f2a3eeade80ff9e3aaf361a362fbe55c
zobirofkir <zobirofkir19@gmail.com>
2025-03-29 22:15:07 +0000

    use cache for performanc

--- app/Services/Services/GithubThemeService.php
+++ app/Services/Services/GithubThemeService.php
@@ -4,11 +4,28 @@ namespace App\Services\Services;
 
 use App\Services\Constructors\GithubThemeConstructor;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class GithubThemeService implements GithubThemeConstructor
 {
+    /**
+     * Cache TTL in seconds (1 hour)
+     */
+    protected $cacheTtl = 3600;
+
     public function index()
     {
+        $cacheKey = 'github_themes_list';
+
+        if (Cache::has($cacheKey)) {
+            return response()->json([
+                'success' => true,
+                'themes' => Cache::get($cacheKey),
+                'source' => 'cache'
+            ]);
+        }
+
         $response = Http::get('https://api.github.com/repos/zobirofkir/wee-build-themes/contents');
 
         if ($response->successful()) {
@@ -31,9 +48,13 @@ class GithubThemeService implements GithubThemeConstructor
                 })
                 ->values();
 
+            // Store in cache
+            Cache::put($cacheKey, $themes, $this->cacheTtl);
+
             return response()->json([
                 'success' => true,
-                'themes' => $themes
+                'themes' => $themes,
+                'source' => 'api'
             ]);
         }
 
@@ -62,18 +83,33 @@ class GithubThemeService implements GithubThemeConstructor
      */
     public function getTestTheme($themeName)
     {
+        $cacheKey = "github_theme_{$themeName}";
+
+        if (Cache::has($cacheKey)) {
+            return response()->json([
+                'success' => true,
+                'theme' => Cache::get($cacheKey),
+                'source' => 'cache'
+            ]);
+        }
+
         $response = Http::get("https://api.github.com/repos/zobirofkir/wee-build-themes/contents/{$themeName}");
 
         if ($response->successful()) {
             $contents = $response->json();
 
+            $themeData = [
+                'name' => $themeName,
+                'files' => $contents,
+                'preview_url' => $this->generateTestUrl($themeName)
+            ];
+
+            Cache::put($cacheKey, $themeData, $this->cacheTtl);
+
             return response()->json([
                 'success' => true,
-                'theme' => [
-                    'name' => $themeName,
-                    'files' => $contents,
-                    'preview_url' => $this->generateTestUrl($themeName)
-                ]
+                'theme' => $themeData,
+                'source' => 'api'
             ]);
         }
 
@@ -81,5 +117,27 @@ class GithubThemeService implements GithubThemeConstructor
             'success' => false,
             'message' => 'Unable to fetch theme details'
         ], 404);
+    }
+
+    /**
+     * Clear the cache for all themes
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function clearCache()
+    {
+        Cache::forget('github_themes_list');
+
+        // Clear individual theme caches
+        $keys = Redis::keys('laravel_cache:github_theme_*');
+        foreach ($keys as $key) {
+            $cacheKey = str_replace('laravel_cache:', '', $key);
+            Cache::forget($cacheKey);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Theme cache cleared successfully'
+        ]);
     }
 }
