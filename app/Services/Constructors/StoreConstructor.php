c7c0f19ba2630b914cd290d148ce975b35977454
zobirofkir <zobirofkir19@gmail.com>
2025-03-29 14:22:16 +0000

    create store service and controller and request and resource

---
+++ app/Services/Constructors/StoreConstructor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Constructors;
+
+interface StoreConstructor
+{
+    //
+}
---
+++ app/Services/Services/StoreService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\Services;
+
+use App\Services\Constructors\StoreConstructor;
+
+class StoreService implements StoreConstructor
+{
+    //
+}
