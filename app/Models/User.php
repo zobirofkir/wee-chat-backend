6cdbb6bb16f93a1e070c7214e5e7a184c2a904b7
zobirofkir <zobirofkir19@gmail.com>
2025-03-29 14:20:15 +0000

    add relation sheep between store and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -61,5 +62,15 @@ class User extends Authenticatable
     public function accessToken() : string
     {
         return $this->createToken('accessToken')->accessToken;
+    }
+
+    /**
+     * Get the stores for the user.
+     *
+     * @return HasMany
+     */
+    public function stores() : HasMany
+    {
+        return $this->hasMany(Store::class);
     }
 }
