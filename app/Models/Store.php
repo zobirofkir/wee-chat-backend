44307c3fc6d158ff06700ea791bd0a42f9da173f
zobirofkir <zobirofkir19@gmail.com>
2025-03-29 14:20:15 +0000

    add relation sheep between store and user

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,14 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Store extends Model
 {
     use HasFactory;
 
+    /**
+     * Fillable fields
+     *
+     * @var array
+     */
     protected $fillable = [
         'user_id',
         'name',
         'domain',
     ];
+
+    /**
+     * Get the user that owns the store
+     *
+     * @return BelongsTo
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
