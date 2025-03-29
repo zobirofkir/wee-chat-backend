2bf2ba1c3fafd8cc02aa29fff8f61b68be057a08
zobirofkir <zobirofkir19@gmail.com>
2025-03-29 14:23:57 +0000

    add Logique in Store Service

--- app/Services/Services/StoreService.php
+++ app/Services/Services/StoreService.php
@@ -2,9 +2,21 @@
 
 namespace App\Services\Services;
 
+use App\Models\Store;
 use App\Services\Constructors\StoreConstructor;
+use Illuminate\Support\Str;
 
 class StoreService implements StoreConstructor
 {
-    //
+    public function createStore($user)
+    {
+        $storeName = "Store of " . $user->name;
+        $domain = Str::slug($user->name) . ".mystore.com";
+
+        return Store::create([
+            'user_id' => $user->id,
+            'name' => $storeName,
+            'domain' => $domain,
+        ]);
+    }
 }
